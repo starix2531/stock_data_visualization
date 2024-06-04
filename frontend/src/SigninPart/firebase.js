125246b17d3b62e635f87a950da0da3ba7f2bcd0
Chenghao Sun <74682270+starix2531@users.noreply.github.com>
2024-06-04 01:57:52 -0700

    Update firebase.js

--- frontend/src/SigninPart/firebase.js
+++ frontend/src/SigninPart/firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBFBPHOhtu1siGQhgtp9nB6JlxX77BEFN8",
-    authDomain: "stock-firebase-v1.firebaseapp.com",
-    projectId: "stock-firebase-v1",
-    storageBucket:  "stock-firebase-v1.appspot.com",
-    messagingSenderId: "95744555465",
-    appId: "1:95744555465:web:91f1cc7a5b1ec41176d018"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket:  "",
+    messagingSenderId: "",
+    appId: ""
   };
 
 const app = initializeApp(firebaseConfig);
